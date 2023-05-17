51fe75c7c4f54aae2da523c2a1d504c2fca517eb
nicugnm <nghergu17@gmail.com>
2023-05-18 01:15:54 +0300

    Add grafana & prometheus configs.

--- src/main/java/ro/nicolaemariusghergu/loginjwt/config/SecurityConfiguration.java
+++ src/main/java/ro/nicolaemariusghergu/loginjwt/config/SecurityConfiguration.java
@@ -9,9 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,10 +24,9 @@ public class SecurityConfiguration {
         httpSecurity.csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
+                .requestMatchers("/api/v1/auth/**", "/actuator/**")
+                .permitAll().anyRequest().authenticated();
+
 
 
         // authorization
